222e9f9e2bdd9b0995cd5efd6d8f7b2ea011deb4
박찬우 <116543427+parkchanwu@users.noreply.github.com>
2024-10-02 18:10:20 +0900

    데이터베이스 연동 성공
    회원가입 스크린 --> 로그인 스크린

--- lib/feature/screen/SignUpPage.dart
+++ lib/feature/screen/SignUpPage.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:attendance_check/database/model/studentModel.dart';
-import 'package:attendance_check/database/model/managerModel.dart';
 
 class SignUpPage extends StatefulWidget {
   @override
@@ -10,54 +7,6 @@ class SignUpPage extends StatefulWidget {
 
 class _SignUpPageState extends State<SignUpPage> {
   final _formKey = GlobalKey<FormState>();
-  String? _selectedRole; // 역할 선택 변수
-  String? _selectedDepartment; // 학과 선택 변수
-  String? _name;
-  String? _studentId;
-  String? _password;
-  String? _tel;
-  DateTime? _birthDate;
-
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
-  // 회원가입 버튼을 눌렀을 때 호출되는 함수
-  Future<void> _submitForm() async {
-    if (_formKey.currentState!.validate()) {
-      _formKey.currentState!.save();
-
-      if (_selectedRole == '학부생') {
-        // 학부생일 경우
-        final student = StudentModel(
-          studentId: _studentId!,
-          department: _selectedDepartment!,
-          name: _name!,
-          tel: _tel!,
-          password: _password!,
-        );
-
-        // Firestore에 학부생 데이터 저장
-        await _firestore.collection('student').add(student.toMap());
-        print('학부생 데이터 저장 완료!');
-      } else if (_selectedRole == '관리자') {
-        // 관리자일 경우
-        final manager = ManagerModel(
-          managerId: _studentId!, // 학번 대신 관리자 ID 사용
-          department: _selectedDepartment!,
-          name: _name!,
-          password: _password!,
-        );
-
-        // Firestore에 관리자 데이터 저장
-        await _firestore.collection('manager').add(manager.toMap());
-        print('관리자 데이터 저장 완료!');
-      }
-
-      // 데이터 저장 완료 후 메시지 혹은 다음 페이지 이동
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('회원가입이 완료되었습니다!')),
-      );
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -65,116 +14,125 @@ class _SignUpPageState extends State<SignUpPage> {
       appBar: AppBar(
         title: Text('회원가입 페이지'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
+      body: SingleChildScrollView(  // 스크롤 가능하게 설정
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // 역할 선택 필드
-              DropdownButtonFormField<String>(
-                decoration: InputDecoration(
-                  labelText: '역할을 선택하세요',
-                  border: OutlineInputBorder(),
-                ),
-                items: ['학부생', '관리자']
-                    .map((value) => DropdownMenuItem(
-                  child: Text(value),
-                  value: value,
-                ))
-                    .toList(),
-                onChanged: (value) {
-                  setState(() {
-                    _selectedRole = value;
-                  });
-                },
-                validator: (value) =>
-                value == null ? '역할을 선택하세요' : null,
-              ),
-              const SizedBox(height: 20),
+              const SizedBox(height: 80),  // 상단 여백 추가
+              Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // 역할 선택 필드
+                    DropdownButtonFormField<String>(
+                      decoration: InputDecoration(
+                        labelText: '역할을 선택하세요',
+                        border: OutlineInputBorder(),
+                      ),
+                      items: ['학부생', '관리자']
+                          .map((value) => DropdownMenuItem(
+                        child: Text(value),
+                        value: value,
+                      ))
+                          .toList(),
+                      onChanged: (value) {
+                        // 역할 선택 로직
+                      },
+                      validator: (value) =>
+                      value == null ? '역할을 선택하세요' : null,
+                    ),
+                    const SizedBox(height: 20),
 
-              // 학과 선택 필드
-              DropdownButtonFormField<String>(
-                decoration: InputDecoration(
-                  labelText: '학과를 선택하세요',
-                  border: OutlineInputBorder(),
-                ),
-                items: ['의료IT공학과', '컴퓨터소프트웨어공학과', '정보보호학과', 'AI빅데이터학과', '사물인터넷학과', '메타버스&게임학과']
-                    .map((value) => DropdownMenuItem(
-                  child: Text(value),
-                  value: value,
-                ))
-                    .toList(),
-                onChanged: (value) {
-                  setState(() {
-                    _selectedDepartment = value;
-                  });
-                },
-                validator: (value) =>
-                value == null ? '학과를 선택하세요' : null,
-              ),
-              const SizedBox(height: 20),
+                    // 학과 선택 필드
+                    DropdownButtonFormField<String>(
+                      decoration: InputDecoration(
+                        labelText: '학과를 선택하세요',
+                        border: OutlineInputBorder(),
+                      ),
+                      items: [
+                        '의료IT공학과',
+                        '컴퓨터소프트웨어공학과',
+                        '정보보호학과',
+                        'AI빅데이터학과',
+                        '사물인터넷학과',
+                        '메타버스&게임학과'
+                      ].map((value) => DropdownMenuItem(
+                        child: Text(value),
+                        value: value,
+                      )).toList(),
+                      onChanged: (value) {
+                        // 학과 선택 로직
+                      },
+                      validator: (value) =>
+                      value == null ? '학과를 선택하세요' : null,
+                    ),
+                    const SizedBox(height: 20),
 
-              // 이름 입력 필드
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: '이름',
-                  border: OutlineInputBorder(),
-                ),
-                onSaved: (value) => _name = value,
-                keyboardType: TextInputType.name, // 이 부분 추가
-                validator: (value) =>
-                value == null || value.isEmpty ? '이름을 입력하세요' : null,
-              ),
-              const SizedBox(height: 20),
+                    // 이름 입력 필드
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: '이름',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) => value == null || value.isEmpty
+                          ? '이름을 입력하세요'
+                          : null,
+                    ),
+                    const SizedBox(height: 20),
 
-              // 학번 또는 관리자 ID 입력 필드
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: _selectedRole == '관리자' ? '관리자 ID' : '학번',
-                  border: OutlineInputBorder(),
-                ),
-                onSaved: (value) => _studentId = value,
-                validator: (value) =>
-                value == null || value.isEmpty ? '학번을 입력하세요' : null,
-              ),
-              const SizedBox(height: 20),
+                    // 학번 입력 필드
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: '학번',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) => value == null || value.isEmpty
+                          ? '학번을 입력하세요'
+                          : null,
+                    ),
+                    const SizedBox(height: 20),
 
-              // 전화번호 입력 필드 (학부생만)
-              if (_selectedRole == '학부생')
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: '전화번호',
-                    border: OutlineInputBorder(),
-                  ),
-                  onSaved: (value) => _tel = value,
-                  validator: (value) =>
-                  value == null || value.isEmpty ? '전화번호를 입력하세요' : null,
-                ),
-              const SizedBox(height: 20),
+                    // 전화번호 입력 필드 (선택 사항)
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: '전화번호 (선택 사항)',
+                        border: OutlineInputBorder(),
+                      ),
+                      // validator를 null로 설정하여 선택적으로 입력 가능하게 함
+                    ),
+                    const SizedBox(height: 20),
 
-              // 비밀번호 입력 필드
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: '비밀번호',
-                  border: OutlineInputBorder(),
-                ),
-                obscureText: true,
-                onSaved: (value) => _password = value,
-                validator: (value) =>
-                value == null || value.isEmpty ? '비밀번호를 입력하세요' : null,
-              ),
-              const SizedBox(height: 40),
+                    // 비밀번호 입력 필드
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: '비밀번호',
+                        border: OutlineInputBorder(),
+                      ),
+                      obscureText: true, // 비밀번호 가리기
+                      validator: (value) => value == null || value.isEmpty
+                          ? '비밀번호를 입력하세요'
+                          : null,
+                    ),
+                    const SizedBox(height: 40),
 
-              // 회원가입 버튼
-              ElevatedButton(
-                onPressed: _submitForm,
-                child: Text('회원가입'),
-                style: ElevatedButton.styleFrom(
-                  minimumSize: Size(double.infinity, 50), // 버튼 가로 크기
+                    // 회원가입 버튼
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          // 회원가입 처리 로직
+                        }
+                      },
+                      child: Text('회원가입'),
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(double.infinity, 50), // 버튼 크기
+                      ),
+                    ),
+                  ],
                 ),
               ),
+              const SizedBox(height: 30),  // 하단 여백 추가
             ],
           ),
         ),
