73a5e8a01fdcf63b7557eb43db6673240c6873ce
Jihee <nblue33@naver.com>
2024-10-04 01:54:23 +0900

    -디자인 변경(로고, 텍스트 필드, 버튼 등)

--- lib/feature/screen/SignUpPage.dart
+++ lib/feature/screen/SignUpPage.dart
@@ -1,7 +1,7 @@
 import 'package:attendance_check/database/db_query/manage_db_query.dart';
 import 'package:attendance_check/database/db_query/student_db_query.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';  // inputFormatters를 사용하기 위한 import
+import 'package:flutter/services.dart'; // inputFormatters를 사용하기 위한 import
 import 'package:cloud_firestore/cloud_firestore.dart'; // Firestore 사용을 위한 import
 import 'package:attendance_check/database/model/studentModel.dart';
 import 'package:attendance_check/database/model/managerModel.dart';
@@ -72,7 +72,7 @@ class _SignUpPageState extends State<SignUpPage> {
             tel: _tel!,
             password: _password!,
           );
-          await _studentService.addStudent(student);  // Firestore에 저장
+          await _studentService.addStudent(student); // Firestore에 저장
         } else if (_selectedRole == '관리자') {
           // 관리자 정보 저장
           final manager = ManagerModel(
@@ -81,7 +81,7 @@ class _SignUpPageState extends State<SignUpPage> {
             name: _name!,
             password: _password!,
           );
-          await _managerService.addManager(manager);  // Firestore에 저장
+          await _managerService.addManager(manager); // Firestore에 저장
         }
 
         // 회원가입 성공 시 팝업을 띄우고 1초 후에 로그인 페이지로 이동
@@ -105,9 +105,20 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('회원가입 페이지'),
+        backgroundColor: Colors.white,
+        title: Padding(
+          padding: EdgeInsets.only(left: 10, top: 20), // Image 주변에 margin을 적용
+          child: Image.asset(
+            'assets/logo.png',
+            height: 100,
+            width: 160,
+            fit: BoxFit.contain,
+          ),
+        ),
       ),
+
       body: SingleChildScrollView(  // 스크롤 가능하게 설정
         child: Padding(
           padding: const EdgeInsets.all(16.0),
@@ -122,7 +133,7 @@ class _SignUpPageState extends State<SignUpPage> {
                     // 역할 선택 필드
                     DropdownButtonFormField<String>(
                       decoration: InputDecoration(
-                        labelText: '역할을 선택하세요',
+                        labelText: '사용자 유형을 선택하세요',
                         border: OutlineInputBorder(),
                       ),
                       items: ['학부생', '관리자']
@@ -136,7 +147,7 @@ class _SignUpPageState extends State<SignUpPage> {
                         });
                       },
                       validator: (value) =>
-                      value == null ? '역할을 선택하세요' : null,
+                      value == null ? '사용자 유형을 선택하세요' : null,
                     ),
                     const SizedBox(height: 20),
 
@@ -153,17 +164,18 @@ class _SignUpPageState extends State<SignUpPage> {
                         'AI빅데이터학과',
                         '사물인터넷학과',
                         '메타버스&게임학과'
-                      ].map((value) => DropdownMenuItem(
-                        child: Text(value),
-                        value: value,
-                      )).toList(),
+                      ]
+                          .map((value) => DropdownMenuItem(
+                                child: Text(value),
+                                value: value,
+                              ))
+                          .toList(),
                       onChanged: (value) {
                         setState(() {
                           _department = value;
                         });
                       },
-                      validator: (value) =>
-                      value == null ? '학과를 선택하세요' : null,
+                      validator: (value) => value == null ? '학과를 선택하세요' : null,
                     ),
                     const SizedBox(height: 20),
 
@@ -176,9 +188,8 @@ class _SignUpPageState extends State<SignUpPage> {
                       onSaved: (value) {
                         _name = value;
                       },
-                      validator: (value) => value == null || value.isEmpty
-                          ? '이름을 입력하세요'
-                          : null,
+                      validator: (value) =>
+                          value == null || value.isEmpty ? '이름을 입력하세요' : null,
                     ),
                     const SizedBox(height: 20),
 
@@ -191,28 +202,11 @@ class _SignUpPageState extends State<SignUpPage> {
                       onSaved: (value) {
                         _studentId = value;
                       },
-                      validator: (value) => value == null || value.isEmpty
-                          ? '학번을 입력하세요'
-                          : null,
+                      validator: (value) =>
+                          value == null || value.isEmpty ? '학번을 입력하세요' : null,
                       keyboardType: TextInputType.number,
                       inputFormatters: [
-                        FilteringTextInputFormatter.digitsOnly,  // 숫자만 입력 가능
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-
-                    // 전화번호 입력 필드 (선택 사항)
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: '전화번호 (선택 사항)',
-                        border: OutlineInputBorder(),
-                      ),
-                      onSaved: (value) {
-                        _tel = value;
-                      },
-                      keyboardType: TextInputType.phone,
-                      inputFormatters: [
-                        FilteringTextInputFormatter.digitsOnly,  // 숫자만 입력 가능
+                        FilteringTextInputFormatter.digitsOnly, // 숫자만 입력 가능
                       ],
                     ),
                     const SizedBox(height: 20),
@@ -220,37 +214,61 @@ class _SignUpPageState extends State<SignUpPage> {
                     // 비밀번호 입력 필드
                     TextFormField(
                       decoration: InputDecoration(
-                        labelText: '비밀번호',
+                        labelText: '생년월일(6자리)',
                         border: OutlineInputBorder(),
                       ),
                       onSaved: (value) {
                         _password = value;
                       },
                       obscureText: true, // 비밀번호 가리기
-                      validator: (value) => value == null || value.isEmpty
-                          ? '비밀번호를 입력하세요'
-                          : null,
+                      validator: (value) =>
+                      value == null || value.isEmpty ? '비밀번호를 입력하세요' : null,
                       keyboardType: TextInputType.number,
                       inputFormatters: [
-                        FilteringTextInputFormatter.digitsOnly,  // 숫자만 입력 가능
+                        FilteringTextInputFormatter.digitsOnly, // 숫자만 입력 가능
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+
+
+                    // 전화번호 입력 필드 (선택 사항)
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: '[선택] 전화번호',
+                        border: OutlineInputBorder(),
+                      ),
+                      onSaved: (value) {
+                        _tel = value;
+                      },
+                      keyboardType: TextInputType.phone,
+                      inputFormatters: [
+                        FilteringTextInputFormatter.digitsOnly, // 숫자만 입력 가능
                       ],
                     ),
                     const SizedBox(height: 40),
 
                     // 회원가입 버튼
-                    ElevatedButton(
-                      onPressed: () {
-                        _submitForm(context);  // 회원가입 처리 로직 호출
-                      },
-                      child: Text('회원가입'),
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: Size(double.infinity, 50), // 버튼 크기
+                    Center( // 버튼을 화면 중앙에 배치
+                      child: ElevatedButton(
+                        onPressed: () {
+                          _submitForm(context); // 회원가입 처리 로직 호출
+                        },
+                        child: Text('회원가입'),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black, // 버튼의 배경색을 검정색으로 설정
+                          foregroundColor: Colors.white, // 버튼 텍스트 색상을 하얀색으로 설정
+                          minimumSize: Size(200, 50), // 버튼 크기 설정
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10), // 버튼 모서리 둥글게
+                          ),
+                        ),
                       ),
-                    ),
+                    )
+
                   ],
                 ),
               ),
-              const SizedBox(height: 30),  // 하단 여백 추가
+              const SizedBox(height: 30), // 하단 여백 추가
             ],
           ),
         ),
