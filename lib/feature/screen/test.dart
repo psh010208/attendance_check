a385b222f2b3296efcdaff43d442198171ea9028
kdo@naver.com <116543097+hekim-cse@users.noreply.github.com>
2024-10-05 00:28:17 +0900

    스케쥴 창 제거, 메뉴바 창 설정

--- lib/feature/screen/test.dart
+++ lib/feature/screen/test.dart
@@ -44,13 +44,91 @@ class _ScheduleScreenState extends State<ScheduleScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Image.asset('assets/logo.png', width: 150, height: 30), // 학교 로고
-            Spacer(),
-            IconButton(onPressed: () {}, icon: Icon(Icons.menu)), // 메뉴 아이콘
+            //IconButton(onPressed: () {
+            //Scaffold.of(context).openDrawer(); // 메뉴 버튼 클릭 시 Drawer 열기
+            //},
+            //  icon: Icon(Icons.menu)), // 메뉴 아이콘  --->  여기
+          ],
+        ),
+      ),
+      endDrawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Center( // Center 위젯으로 변경
+                child: Text(
+                  '내 정보',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                  ),
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Color(0xff0d47a1),
+              ),
+            ),
+            // 첫 번째 ListTile 정의
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text(
+                '학생 이름', // 여기에 학생 이름을 추가하세요
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                ),
+              ),
+              // subtitle: Text('${student.department} - ${student.name}'), // 학과와 이름 추가
+              subtitle: Text(
+                '00학과 000',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 10,
+                ),
+              ),
+            ),
+            // 두 번째 ListTile 정의
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text(
+                '학생 이름', // 여기에 학생 이름을 추가하세요
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                ),
+              ),
+              // subtitle: Text('${student.department} - ${student.name}'), // 학과와 이름 추가
+              subtitle: Text(
+                '00학과 000',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 10,
+                ),
+              ),
+            ),
+            // '밀어서 닫기' 텍스트 추가
+            Padding(
+              padding: const EdgeInsets.only(top: 300.0), // 원하는 간격 조정
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.of(context).pop(); // Drawer 닫기
+                },
+                child: Text(
+                  '밀어서 닫기',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    decoration: TextDecoration.underline,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
           ],
         ),
       ),
