45745b889b8c794594e201c6312949278060caec
박찬우 <116543427+parkchanwu@users.noreply.github.com>
2024-10-13 06:54:35 +0900

    테블릿 스크린으로 화면 규격 테스트 -->

--- lib/feature/Log/widget/CustomDropdownFormField.dart
+++ lib/feature/Log/widget/CustomDropdownFormField.dart
@@ -18,40 +18,44 @@ class CustomDropdownFormField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButtonFormField<String>(
-      decoration: InputDecoration(
-        labelText: labelText,
-        labelStyle: Theme.of(context).textTheme.titleSmall?.copyWith(
-          color: Theme.of(context).colorScheme.primary,
-          fontWeight: FontWeight.bold, // 굵게
-          fontSize: 19.sp, // 반응형 폰트 크기
-        ),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10.0), // 둥근 테두리
-          borderSide: BorderSide(
+    return Padding( // 패딩을 사용하여 여유 공간을 추가
+      padding: EdgeInsets.symmetric(vertical: 10.h), // 상하 여백 추가
+      child: DropdownButtonFormField<String>(
+        decoration: InputDecoration(
+          labelText: labelText,
+          labelStyle: Theme.of(context).textTheme.titleSmall?.copyWith(
             color: Theme.of(context).colorScheme.primary,
-            width: 2.0.w, // 테두리 두께
+            fontWeight: FontWeight.bold, // 굵게
+            fontSize: 19.sp, // 반응형 폰트 크기
           ),
-        ),
-      ),
-      value: value,
-      dropdownColor: Theme.of(context).colorScheme.onInverseSurface,
-      borderRadius: BorderRadius.circular(10.0), // 둥근 테두리
-      items: items.map((item) {
-        return DropdownMenuItem(
-          child: Text(
-            item,
-            style: Theme.of(context).textTheme.titleSmall?.copyWith(
-              fontWeight: FontWeight.bold, // 굵게
-              color: Theme.of(context).hintColor,
-              fontSize: 18.sp, // 반응형 폰트 크기
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10.0), // 둥근 테두리
+            borderSide: BorderSide(
+              color: Theme.of(context).colorScheme.primary,
+              width: 2.0.w, // 테두리 두께
             ),
           ),
-          value: item,
-        );
-      }).toList(),
-      onChanged: onChanged,
-      validator: validator,
+        ),
+        value: value,
+        dropdownColor: Theme.of(context).colorScheme.onInverseSurface,
+        borderRadius: BorderRadius.circular(10.0), // 둥근 테두리
+        isExpanded: true,  // Dropdown 선택 항목이 잘리지 않도록 확장
+        items: items.map((item) {
+          return DropdownMenuItem(
+            child: Text(
+              item,
+              style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                fontWeight: FontWeight.bold, // 굵게
+                color: Theme.of(context).hintColor,
+                fontSize: 18.sp, // 반응형 폰트 크기
+              ),
+            ),
+            value: item,
+          );
+        }).toList(),
+        onChanged: onChanged,
+        validator: validator,
+      ),
     );
   }
 }
--- lib/feature/Log/widget/SignUpForm.dart
+++ lib/feature/Log/widget/SignUpForm.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:attendance_check/database/Repository/StudentRepository.dart';
-import 'package:attendance_check/database/Repository/ManagerRepository.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart'; // Use existing ScreenUtil
-import '../../Home/widget/SoonCheck.dart'; // SoonCheck import
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../Home/widget/SoonCheck.dart';
+import 'CustomDropdownFormField.dart';
 import 'CustomTextFormField.dart';
 import 'LogUpButton.dart';
-import 'CustomDropdownFormField.dart'; // CustomDropdownFormField import
 
 class SignUpForm extends StatefulWidget {
   @override
@@ -15,12 +13,9 @@ class SignUpForm extends StatefulWidget {
 
 class _SignUpFormState extends State<SignUpForm> {
   final _formKey = GlobalKey<FormState>();
-  final _studentRepository = StudentRepository();
-  final _managerRepository = ManagerRepository();
-
   String? _selectedRole = '학부생';
-  String? _department = '의료IT공학과'; // 기본값을 '의료IT공학과'로 설정
-  String? _name, _studentId, _tel, _password;
+  String? _department = '의료IT공학과';
+  String? _name, _studentId;
 
   Future<void> _submitForm(BuildContext context) async {
     if (!_formKey.currentState!.validate()) return;
@@ -31,103 +26,76 @@ class _SignUpFormState extends State<SignUpForm> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          SoonCheckWidget(bottom: 600.h, left: -25.w), // Use ScreenUtil to make responsive
-          Padding(
-            padding: EdgeInsets.only(top: 250.h), // Responsive padding
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30.w), // Responsive horizontal padding
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start, // Align form at the top
-                crossAxisAlignment: CrossAxisAlignment.stretch, // Stretch form fields to full width
-                children: [
-                  // 사용자 유형 Dropdown
-                  CustomDropdownFormField(
-                    labelText: '사용자 유형',
-                    value: _selectedRole,
-                    items: ['학부생', '관리자'],
-                    onChanged: (value) {
-                      setState(() {
-                        _selectedRole = value;
-                      });
-                    },
-                    validator: (value) => value == null ? '사용자 유형을 선택하세요' : null,
-                  ),
-                  SizedBox(height: 30.h), // Responsive spacing
-
-                  // 학부생을 선택한 경우에만 학과 선택 필드를 보여줌
-                  Visibility(
-                    visible: _selectedRole == '학부생', // Show only if 학부생 is selected
-                    child: Column(
-                      children: [
-                        CustomDropdownFormField(
-                          labelText: '학과를 선택하세요',
-                          value: _department,
-                          items: [
-                            '의료IT공학과',
-                            '컴퓨터소프트웨어공학과',
-                            '정보보호학과',
-                            'AI빅데이터학과',
-                            '사물인터넷학과',
-                            '메타버스&게임학과'
-                          ],
-                          onChanged: (value) {
-                            setState(() {
-                              _department = value;
-                            });
-                          },
-                          validator: (value) => value == null ? '학과를 선택하세요' : null,
-                        ),
-                        SizedBox(height: 30.h), // Responsive spacing
-                      ],
-                    ),
-                  ),
-
-                  // 이름 TextField
-                  buildCustomTextField('이름', TextInputType.text, false,
-                          (value) => _name = value, '이름을 입력하세요'),
-                  SizedBox(height: 30.h), // Responsive spacing
-
-                  // 학번 TextField
-                  buildCustomTextField('학번', TextInputType.number, false,
-                          (value) => _studentId = value, '학번을 입력하세요', FilteringTextInputFormatter.digitsOnly),
-
-                  // 학부생일 때는 위에 학과 선택 필드가 보이기 때문에 더 넓은 간격 추가
-                  SizedBox(height: _selectedRole == '관리자' ? 80.h : 100.h), // Responsive spacing
-
-                  // 회원가입 Button
-                  Center(
-                    child: LogUpButton(
-                      onPressed: () => _submitForm(context),
-                      text: '회원가입',
-                    ),
-                  ),
-                ],
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30.w), // 좌우 여백을 고정
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start, // 위쪽에 배치
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(height: 40.h), // 상단에 빈 공간 추가
+              Flexible(
+                child: SoonCheckWidget(bottom: -10.h, left: -50.w), // 반응형 위치 설정
               ),
-            ),
+              SizedBox(height: 40.h), // 간격 유지
+              CustomDropdownFormField(
+                labelText: '사용자 유형',
+                value: _selectedRole,
+                items: ['학부생', '관리자'],
+                onChanged: (value) {
+                  setState(() {
+                    _selectedRole = value;
+                  });
+                },
+              ),
+              SizedBox(height: 20.h), // 간격 유지
+              if (_selectedRole == '학부생')
+                CustomDropdownFormField(
+                  labelText: '학과를 선택하세요',
+                  value: _department,
+                  items: [
+                    '의료IT공학과',
+                    '컴퓨터소프트웨어공학과',
+                    '정보보호학과',
+                    'AI빅데이터학과',
+                    '사물인터넷학과',
+                    '메타버스&게임학과'
+                  ],
+                  onChanged: (value) {
+                    setState(() {
+                      _department = value;
+                    });
+                  },
+                ),
+              SizedBox(height: 20.h), // 간격 유지
+              buildCustomTextField('이름', TextInputType.text, false, (value) => _name = value),
+              SizedBox(height: 20.h),
+              buildCustomTextField('학번', TextInputType.number, false, (value) => _studentId = value),
+              Spacer(), // 남은 공간을 밀어주기 위해 사용
+              Flexible(
+                child: Center(
+                  child: LogUpButton(
+                    onPressed: () => _submitForm(context),
+                    text: '회원가입',
+                  ),
+                ),
+              ),
+              SizedBox(height: 130.h), // 버튼 아래 여백 추가
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
 
   // Custom TextField widget to avoid conflicts
-  Widget buildCustomTextField(
-      String labelText,
-      TextInputType keyboardType,
-      bool obscureText,
-      FormFieldSetter<String>? onSaved,
-      String? validatorMessage, [
-        TextInputFormatter? inputFormatter,
-      ]) {
+  Widget buildCustomTextField(String labelText, TextInputType keyboardType, bool obscureText, FormFieldSetter<String>? onSaved) {
     return InfoTextField(
       labelText: labelText,
       keyboardType: keyboardType,
       obscureText: obscureText,
-      inputFormatters: inputFormatter != null ? [inputFormatter] : null,
       onSaved: onSaved,
-      validator: (value) => value == null || value.isEmpty ? validatorMessage : null,
+      validator: (value) => value == null || value.isEmpty ? '$labelText을 입력하세요' : null,
     );
   }
 }
