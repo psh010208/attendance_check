5f641da136d191a3842837790f358de02638c6af
psh <psh010208@naver.com>
2024-10-15 22:39:12 +0900

    승인 대기 현황 페이지 수정

--- lib/feature/CurrentList/ApproveWaitingList.dart
+++ lib/feature/CurrentList/ApproveWaitingList.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'WaitingData.dart';
 import 'package:attendance_check/feature/Drawer/drawerScreen.dart';
 import 'package:attendance_check/feature/Home/homeScreen.dart';
@@ -6,16 +7,15 @@ import 'package:attendance_check/feature/Home/homeScreen.dart';
 class ApproveWaitingList extends StatelessWidget {
   final String role;
   final String id;
+
   ApproveWaitingList(this.role, this.id);
 
   @override
   Widget build(BuildContext context) {
     print("role: $role, id: $id");
 
-    return MaterialApp(
-          home: Scaffold(
-            body: WaitingStatus(role: role, id: id), // role과 id 전달
-          ),
+    return Scaffold(
+      body: WaitingStatus(role: role, id: id), // role과 id 전달
     );
   }
 }
@@ -32,7 +32,8 @@ class WaitingStatus extends StatefulWidget {
 
 class _WaitingStatusState extends State<WaitingStatus> {
   List<Waitinglist> _data = List.from(waitinglist);
-  List<Waitinglist> _originalData = List.from(waitinglist); // 원본 데이터 저장(검색 기능 때문)
+  List<Waitinglist> _originalData =
+      List.from(waitinglist); // 원본 데이터 저장(검색 기능 때문)
   bool _isSortAsc = true; // 정렬 기능(승인 여부에서 사용)
   String _searchStudentNum = ""; // 사번 검색 기능
   String _searchName = ""; // 이름 검색 기능
@@ -43,61 +44,52 @@ class _WaitingStatusState extends State<WaitingStatus> {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
 
-
-    print("Role in WaitingStatus: ${widget.role}, ID: ${widget.id}"); // 전달된 값 확인
+    print(
+        "Role in WaitingStatus: ${widget.role}, ID: ${widget.id}"); // 전달된 값 확인
 
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(45), // AppBar의 높이 설정
-        child: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.surface,
-          iconTheme: IconThemeData(
-            color: Theme.of(context).colorScheme.surface,
-          ),
-          title: Text(
-            '승인 대기 명단',
-            style: Theme.of(context).textTheme.titleMedium,
-          ),
-          centerTitle: true,
-          elevation: 1,
-          leading: IconButton(
-              icon: Icon(Icons.arrow_back,
-                  color: Theme.of(context).colorScheme.onSurface),
-              onPressed: () {
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            HomeScreen(role: widget.role, id: widget.id)));
-              }),
-          actions: [
-            Builder(
-              builder: (context) {
-                return IconButton(
-                  icon: Icon(Icons.menu),
-                  onPressed: () {
-                    Scaffold.of(context).openEndDrawer();
-                  },
-                  color: Theme.of(context).colorScheme.onSurface,
-                );
-              },
-            ),
-          ],
-          shape: Border(
-            bottom: BorderSide(
-              color: Colors.grey,
-              width: 1,
-            ),
-          ),
+      appBar: AppBar(
+        toolbarHeight: 50.h, // 반응형으로 상단바의 높이를 설정
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        iconTheme: IconThemeData(
+          color: Theme.of(context).colorScheme.surface,
         ),
+        title: Text(
+          '승인 대기 현황',
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        centerTitle: true,
+        elevation: 1,
+        leading: IconButton(
+            icon: Icon(Icons.arrow_back,
+                color: Theme.of(context).colorScheme.onSurface),
+            onPressed: () {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          HomeScreen(role: widget.role, id: widget.id)));
+            }),
+        actions: [
+          Builder(
+            builder: (context) {
+              return IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () {
+                  Scaffold.of(context).openEndDrawer();
+                },
+                color: Theme.of(context).colorScheme.onSurface,
+              );
+            },
+          ),
+        ],
       ),
       endDrawer: DrawerScreen(role: widget.role, id: widget.id),
       body: _buildUI(screenHeight, screenWidth),
     );
   }
 
-  Widget _buildUI(double screenHeight, double screenWidth)
-  {
+  Widget _buildUI(double screenHeight, double screenWidth) {
     return RefreshIndicator(
       onRefresh: _refresh,
       backgroundColor: Color(0xffF8FAFD),
@@ -126,10 +118,11 @@ class _WaitingStatusState extends State<WaitingStatus> {
   // 표 제목
   List<DataColumn> _createColumns() {
     return [
-      DataColumn( //아이콘 자리(빈 column)
+      DataColumn(
+        //아이콘 자리(빈 column)
         label: Container(
-          width: 30,
-          height: 30,
+          width: 30.w,
+          height: 30.h,
           child: Row(
             children: [],
           ),
@@ -139,18 +132,18 @@ class _WaitingStatusState extends State<WaitingStatus> {
         label: GestureDetector(
           onTap: () => _showSearchDialog('사번'), // 학번 검색 다이얼로그
           child: Container(
-            width: 85,
-            height: 30,
+            width: 85.w,
+            height: 30.h,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(4),
               color: Color(0xffE2E6EB),
             ),
             child: Row(
               children: [
-                SizedBox(width: 10),
-                Icon(Icons.search, size: 17),
-                SizedBox(width: 6),
-                Text("사번", style: TextStyle(fontSize: 15)),
+                SizedBox(width: 10.w),
+                Icon(Icons.search, size: 17.w),
+                SizedBox(width: 6.w),
+                Text("사번", style: TextStyle(fontSize: 15.sp)),
               ],
             ),
           ),
@@ -160,18 +153,18 @@ class _WaitingStatusState extends State<WaitingStatus> {
         label: GestureDetector(
           onTap: () => _showSearchDialog('이름'), // 이름 검색 다이얼로그
           child: Container(
-            width: 75,
-            height: 30,
+            width: 75.w,
+            height: 30.h,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(4),
               color: Color(0xffE2E6EB),
             ),
             child: Row(
               children: [
-                SizedBox(width: 8),
-                Icon(Icons.search, size: 17),
-                SizedBox(width: 6),
-                Text("이름", style: TextStyle(fontSize: 15)),
+                SizedBox(width: 8.w),
+                Icon(Icons.search, size: 17.w),
+                SizedBox(width: 6.w),
+                Text("이름", style: TextStyle(fontSize: 15.sp)),
               ],
             ),
           ),
@@ -179,39 +172,43 @@ class _WaitingStatusState extends State<WaitingStatus> {
       ),
       DataColumn(
         label: Container(
-          width: 98,
-          height: 30,
+          width: 98.w,
+          height: 30.h,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(4),
             color: Color(0xffE2E6EB),
           ),
           child: Row(
             children: [
-              SizedBox(width: 3),
-              Icon(Icons.arrow_drop_down, size: 25),
-              Text("승인 여부", style: TextStyle(fontSize: 15)),
+              SizedBox(width: 3.w),
+              Icon(Icons.arrow_drop_down, size: 25.w),
+              Text("승인 여부", style: TextStyle(fontSize: 15.sp)),
             ],
           ),
         ),
-        onSort: (columnIndex, _) {  //승인 여부 정렬(문자열 Y랑 N으로)
+        onSort: (columnIndex, _) {
+          //승인 여부 정렬(문자열 Y랑 N으로)
           setState(() {
             if (_isSortAsc) {
               _data.sort(
-                    (a, b) => a.approved.toString().compareTo(b.approved.toString()),
+                (a, b) =>
+                    a.approved.toString().compareTo(b.approved.toString()),
               );
             } else {
               _data.sort(
-                    (a, b) => b.approved.toString().compareTo(a.approved.toString()),
+                (a, b) =>
+                    b.approved.toString().compareTo(a.approved.toString()),
               );
             }
             _isSortAsc = !_isSortAsc;
           });
         },
       ),
-      DataColumn( //승인 버튼 자리(빈 column)
+      DataColumn(
+        //승인 버튼 자리(빈 column)
         label: Container(
-          width: 20,
-          height: 30,
+          width: 20.w,
+          height: 30.h,
           child: Row(
             children: [],
           ),
@@ -266,26 +263,29 @@ class _WaitingStatusState extends State<WaitingStatus> {
   List<DataRow> _createRows(double screenHeight, double screenWidth) {
     return _data
         .where((e) =>
-    e.num.contains(_searchStudentNum) && e.name.contains(_searchName))
+            e.num.contains(_searchStudentNum) && e.name.contains(_searchName))
         .map((e) {
       return DataRow(
         cells: [
-          DataCell(Row(children: [  //아이콘만
-            Icon(Icons.person, size: 17),
+          DataCell(Row(children: [
+            //아이콘만
+            Icon(Icons.person, size: 17.w),
           ])),
           DataCell(Row(children: [
-            SizedBox(width: 10),
-            Text(e.num, style: TextStyle(fontSize: 15)),
+            SizedBox(width: 10.w),
+            Text(e.num, style: TextStyle(fontSize: 15.sp)),
           ])),
           DataCell(Row(children: [
-            SizedBox(width: 15),
-            Text(e.name, style: TextStyle(fontSize: 15)),
+            SizedBox(width: 15.w),
+            Text(e.name, style: TextStyle(fontSize: 15.sp)),
           ])),
-          DataCell(Row(children: [  //승인 여부를 문자열 Y와 N으로 출력
-            SizedBox(width: 40),
-            Text(e.approved ? 'Y' : 'N', style: TextStyle(fontSize: 15)),
+          DataCell(Row(children: [
+            //승인 여부를 문자열 Y와 N으로 출력
+            SizedBox(width: 40.w),
+            Text(e.approved ? 'Y' : 'N', style: TextStyle(fontSize: 15.sp)),
           ])),
-          DataCell(Row(children: [  //승인 버튼
+          DataCell(Row(children: [
+            //승인 버튼
             IconButton(
               icon: Icon(Icons.add_circle, color: Color(0xff26539C)),
               onPressed: () =>
@@ -309,18 +309,27 @@ class _WaitingStatusState extends State<WaitingStatus> {
             width: MediaQuery.of(context).size.width * 0.8, // 화면의 80% 너비
             child: Text(
               "관리자로 승인하시겠습니까?",
-              style: TextStyle(fontSize: 18, color: Colors.black),
+              style: TextStyle(fontSize: 18.sp, color: Colors.black),
             ),
           ),
           actions: [
             TextButton(
-              child: Text("아니요", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17, color: Colors.black)),
+              child: Text("아니요",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17.sp,
+                      color: Colors.black)),
               onPressed: () {
                 Navigator.of(context).pop();
               },
             ),
-            TextButton( //"예"버튼 클릭하면 승인 처리(WaitingData.dart에 approved를 true로 바꿈)
-              child: Text("예", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17, color: Colors.black)),
+            TextButton(
+              //"예"버튼 클릭하면 승인 처리(WaitingData.dart에 approved를 true로 바꿈)
+              child: Text("예",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17.sp,
+                      color: Colors.black)),
               onPressed: () {
                 setState(() {
                   // 승인 처리 로직
