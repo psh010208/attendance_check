6c7c250c505941ad5cbaa637820a31f9fc112149
psh <psh010208@naver.com>
2024-10-15 22:39:24 +0900

    참여 학생 현황 페이지 수정

--- lib/feature/CurrentList/ParticipationStatus.dart
+++ lib/feature/CurrentList/ParticipationStatus.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 // import 'model/model.dart';
 import 'ParticipationData.dart';
 import 'package:attendance_check/feature/Drawer/drawerScreen.dart';
@@ -27,49 +29,36 @@ class _StudentDataTableState extends State<ParticipationStatus> {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
 
-
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(45), // AppBar의 높이 설정
-        child: AppBar(
-          backgroundColor: Color(0xffF8FAFD),
-          title: Text(
-            '참여 학생 명단',
-            style: Theme.of(context).textTheme.titleMedium,
+      appBar: AppBar(
+        toolbarHeight: 50.h, // 반응형으로 상단바의 높이를 설정
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        title: Text('참여 학생 현황', style: Theme.of(context).textTheme.titleMedium),
+        centerTitle: true,
+        elevation: 1,
+        leading: IconButton(
+            icon: Icon(Icons.arrow_back,
+                color: Theme.of(context).colorScheme.onSurface),
+            onPressed: () {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          HomeScreen(role: widget.role, id: widget.id)));
+            }),
+        actions: [
+          Builder(
+            builder: (context) {
+              return IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () {
+                  Scaffold.of(context).openEndDrawer();
+                },
+                color: Theme.of(context).colorScheme.onSurface,
+              );
+            },
           ),
-          centerTitle: true,
-          // iOS에서 제목 중앙 정렬
-          elevation: 1,
-          leading: IconButton(
-              icon: Icon(Icons.arrow_back,
-                  color: Theme.of(context).colorScheme.onSurface),
-              onPressed: () {
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            HomeScreen(role: widget.role, id: widget.id)));
-              }),
-          actions: [
-            Builder(
-              builder: (context) {
-                return IconButton(
-                  icon: Icon(Icons.menu),
-                  onPressed: () {
-                    Scaffold.of(context).openEndDrawer();
-                  },
-                  color: Theme.of(context).colorScheme.onSurface,
-                );
-              },
-            ),
-          ],
-          shape: Border(
-            bottom: BorderSide(
-              color: Colors.grey,
-              width: 1,
-            ),
-          ),
-        ),
+        ],
       ),
       endDrawer: DrawerScreen(role: widget.role, id: widget.id),
       body: _buildUI(screenHeight, screenWidth),
@@ -77,15 +66,15 @@ class _StudentDataTableState extends State<ParticipationStatus> {
   }
 
   Widget _buildUI(double screenHeight, double screenWidth) {
-    return RefreshIndicator(  //아래로 당기면 새로고침
+    return RefreshIndicator(
+      //아래로 당기면 새로고침
       onRefresh: _refresh,
-      backgroundColor: Color(0xffF8FAFD),
       child: SingleChildScrollView(
         scrollDirection: Axis.vertical, //스크롤
         child: FittedBox(
           fit: BoxFit.fitWidth,
           child: DataTable(
-            columnSpacing: 8, //열 사이 간격
+            columnSpacing: 1, //열 사이 간격
             columns: _createColumns(),
             rows: _createRows(screenHeight, screenWidth),
           ),
@@ -105,10 +94,11 @@ class _StudentDataTableState extends State<ParticipationStatus> {
 // 표 제목
   List<DataColumn> _createColumns() {
     return [
-      DataColumn( //아이콘용 빈 column
+      DataColumn(
+        //아이콘용 빈 column
         label: Container(
-          width: 20,
-          height: 30,
+          width: 20.w,
+          height: 30.h,
           child: Row(
             children: [],
           ),
@@ -116,28 +106,29 @@ class _StudentDataTableState extends State<ParticipationStatus> {
       ),
       DataColumn(
         label: Container(
-          width: 145,
-          height: 30,
+          width: 145.w,
+          height: 30.h,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(4),
             color: Color(0xffE2E6EB),
           ),
           child: Row(children: [
-            SizedBox(width: 10),
-            Icon(Icons.arrow_drop_down, size: 25),
-            SizedBox(width: 5),
-            Text("학과", style: TextStyle(fontSize: 15)),
+            SizedBox(width: 10.w),
+            Icon(Icons.arrow_drop_down, size: 25.w),
+            SizedBox(width: 5.w),
+            Text("학과", style: TextStyle(fontSize: 15.sp)),
           ]),
         ),
-        onSort: (columnIndex, _) {  // 학과 정렬
+        onSort: (columnIndex, _) {
+          // 학과 정렬
           setState(() {
             if (_isSortAsc) {
               _data.sort(
-                    (a, b) => a.dept.compareTo(b.dept),
+                (a, b) => a.dept.compareTo(b.dept),
               );
             } else {
               _data.sort(
-                    (a, b) => b.dept.compareTo(a.dept),
+                (a, b) => b.dept.compareTo(a.dept),
               );
             }
             _isSortAsc = !_isSortAsc;
@@ -148,18 +139,18 @@ class _StudentDataTableState extends State<ParticipationStatus> {
         label: GestureDetector(
           onTap: () => _showSearchDialog('학번'), // 학번 검색 다이얼로그
           child: Container(
-            width: 80,
-            height: 30,
+            width: 80.w,
+            height: 30.h,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(4),
               color: Color(0xffE2E6EB),
             ),
             child: Row(
               children: [
-                SizedBox(width: 10),
-                Icon(Icons.search, size: 17),
-                SizedBox(width: 6),
-                Text("학번", style: TextStyle(fontSize: 15)),
+                SizedBox(width: 10.w),
+                Icon(Icons.search, size: 17.sp),
+                SizedBox(width: 6.w),
+                Text("학번", style: TextStyle(fontSize: 15.sp)),
               ],
             ),
           ),
@@ -169,18 +160,18 @@ class _StudentDataTableState extends State<ParticipationStatus> {
         label: GestureDetector(
           onTap: () => _showSearchDialog('이름'), // 이름 검색 다이얼로그
           child: Container(
-            width: 70,
-            height: 30,
+            width: 70.w,
+            height: 30.h,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(4),
               color: Color(0xffE2E6EB),
             ),
             child: Row(
               children: [
-                SizedBox(width: 8),
-                Icon(Icons.search, size: 17),
-                SizedBox(width: 6),
-                Text("이름", style: TextStyle(fontSize: 15)),
+                SizedBox(width: 8.w),
+                Icon(Icons.search, size: 17.w),
+                SizedBox(width: 6.w),
+                Text("이름", style: TextStyle(fontSize: 15.sp)),
               ],
             ),
           ),
@@ -188,30 +179,31 @@ class _StudentDataTableState extends State<ParticipationStatus> {
       ),
       DataColumn(
         label: Container(
-          width: 95,
-          height: 30,
+          width: 95.w,
+          height: 30.h,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(4),
             color: Color(0xffE2E6EB),
           ),
           child: Row(
             children: [
-              SizedBox(width: 2),
-              Icon(Icons.arrow_drop_down, size: 25),
-              SizedBox(width: 4),
-              Text("참여횟수", style: TextStyle(fontSize: 15)),
+              SizedBox(width: 2.w),
+              Icon(Icons.arrow_drop_down, size: 25.w),
+              SizedBox(width: 4.w),
+              Text("참여횟수", style: TextStyle(fontSize: 15.sp)),
             ],
           ),
         ),
-        onSort: (columnIndex, _) {  //참여횟수 정렬
+        onSort: (columnIndex, _) {
+          //참여횟수 정렬
           setState(() {
             if (_isSortAsc) {
               _data.sort(
-                    (a, b) => a.count.compareTo(b.count),
+                (a, b) => a.count.compareTo(b.count),
               );
             } else {
               _data.sort(
-                    (a, b) => b.count.compareTo(a.count),
+                (a, b) => b.count.compareTo(a.count),
               );
             }
             _isSortAsc = !_isSortAsc;
@@ -268,35 +260,37 @@ class _StudentDataTableState extends State<ParticipationStatus> {
     // 필터링 추가
     return _data
         .where((e) =>
-    e.num.contains(_searchStudentNum) && e.name.contains(_searchName))
+            e.num.contains(_searchStudentNum) && e.name.contains(_searchName))
         .map((e) {
       return DataRow(
         cells: [
-          DataCell(Row(children: [  //아이콘만
-            Icon(Icons.person, size: 17),
+          DataCell(Row(children: [
+            //아이콘만
+            Icon(Icons.person, size: 17.w),
           ])),
           DataCell(Row(children: [
-            SizedBox(width: 5),
-            Text(e.dept, style: TextStyle(fontSize: 13)),
+            SizedBox(width: 5.w),
+            Text(e.dept, style: TextStyle(fontSize: 13.sp)),
           ])),
           DataCell(Row(children: [
-            SizedBox(width: 10),
-            Text(e.num, style: TextStyle(fontSize: 13)),
+            SizedBox(width: 10.w),
+            Text(e.num, style: TextStyle(fontSize: 13.sp)),
           ])),
           DataCell(Row(children: [
-            SizedBox(width: 15),
-            Text(e.name, style: TextStyle(fontSize: 13)),
+            SizedBox(width: 15.w),
+            Text(e.name, style: TextStyle(fontSize: 13.sp)),
           ])),
           DataCell(Row(children: [
-            SizedBox(width: 30),
-            Text(e.count, style: TextStyle(fontSize: 13)),
+            SizedBox(width: 30.w),
+            Text(e.count, style: TextStyle(fontSize: 13.sp)),
             IconButton(
               icon: Icon(Icons.keyboard_arrow_down),
               onPressed: () {
                 showDialog(
                   context: context,
                   builder: (BuildContext context) {
-                    return AlertDialog( //일정별 정확한 입퇴실 시간 보여줌
+                    return AlertDialog(
+                      //일정별 정확한 입퇴실 시간 보여줌
                       backgroundColor: Color(0xff26539C),
                       content: SingleChildScrollView(
                         child: Container(
@@ -311,10 +305,10 @@ class _StudentDataTableState extends State<ParticipationStatus> {
                               Divider(),
                               Text('입실   ' + e.time,
                                   style: TextStyle(color: Colors.white)),
-                              SizedBox(height: 5),
+                              SizedBox(height: 5.h),
                               Text('퇴실   2024 / 10 / 9 / 16:30',
                                   style: TextStyle(color: Colors.white)),
-                              SizedBox(height: 10),
+                              SizedBox(height: 10.h),
                               Text('일정 2',
                                   style: TextStyle(
                                       color: Colors.white,
@@ -322,10 +316,10 @@ class _StudentDataTableState extends State<ParticipationStatus> {
                               Divider(),
                               Text('입실   ' + e.time,
                                   style: TextStyle(color: Colors.white)),
-                              SizedBox(height: 5),
+                              SizedBox(height: 5.h),
                               Text('퇴실   2024 / 10 / 9 / 16:30',
                                   style: TextStyle(color: Colors.white)),
-                              SizedBox(height: 10),
+                              SizedBox(height: 10.h),
                               Text('일정 3',
                                   style: TextStyle(
                                       color: Colors.white,
@@ -333,10 +327,10 @@ class _StudentDataTableState extends State<ParticipationStatus> {
                               Divider(),
                               Text('입실   ' + e.time,
                                   style: TextStyle(color: Colors.white)),
-                              SizedBox(height: 5),
+                              SizedBox(height: 5.h),
                               Text('퇴실   2024 / 10 / 9 / 16:30',
                                   style: TextStyle(color: Colors.white)),
-                              SizedBox(height: 10),
+                              SizedBox(height: 10.h),
                               Text('일정 4',
                                   style: TextStyle(
                                       color: Colors.white,
@@ -344,10 +338,10 @@ class _StudentDataTableState extends State<ParticipationStatus> {
                               Divider(),
                               Text('입실   ' + e.time,
                                   style: TextStyle(color: Colors.white)),
-                              SizedBox(height: 5),
+                              SizedBox(height: 5.h),
                               Text('퇴실   2024 / 10 / 9 / 16:30',
                                   style: TextStyle(color: Colors.white)),
-                              SizedBox(height: 10),
+                              SizedBox(height: 10.h),
                               Text('일정 5',
                                   style: TextStyle(
                                       color: Colors.white,
@@ -355,10 +349,10 @@ class _StudentDataTableState extends State<ParticipationStatus> {
                               Divider(),
                               Text('입실   ' + e.time,
                                   style: TextStyle(color: Colors.white)),
-                              SizedBox(height: 5),
+                              SizedBox(height: 5.h),
                               Text('퇴실   2024 / 10 / 9 / 16:30',
                                   style: TextStyle(color: Colors.white)),
-                              SizedBox(height: 10),
+                              SizedBox(height: 10.h),
                               Text('일정 6',
                                   style: TextStyle(
                                       color: Colors.white,
@@ -366,10 +360,10 @@ class _StudentDataTableState extends State<ParticipationStatus> {
                               Divider(),
                               Text('입실   ' + e.time,
                                   style: TextStyle(color: Colors.white)),
-                              SizedBox(height: 5),
+                              SizedBox(height: 5.h),
                               Text('퇴실   2024 / 10 / 9 / 16:30',
                                   style: TextStyle(color: Colors.white)),
-                              SizedBox(height: 10),
+                              SizedBox(height: 10.h),
                               Text('일정 7',
                                   style: TextStyle(
                                       color: Colors.white,
@@ -377,10 +371,10 @@ class _StudentDataTableState extends State<ParticipationStatus> {
                               Divider(),
                               Text('입실   ' + e.time,
                                   style: TextStyle(color: Colors.white)),
-                              SizedBox(height: 5),
+                              SizedBox(height: 5.h),
                               Text('퇴실   2024 / 10 / 9 / 16:30',
                                   style: TextStyle(color: Colors.white)),
-                              SizedBox(height: 10),
+                              SizedBox(height: 10.h),
                               Text('일정 8',
                                   style: TextStyle(
                                       color: Colors.white,
@@ -388,10 +382,10 @@ class _StudentDataTableState extends State<ParticipationStatus> {
                               Divider(),
                               Text('입실   ' + e.time,
                                   style: TextStyle(color: Colors.white)),
-                              SizedBox(height: 5),
+                              SizedBox(height: 5.h),
                               Text('퇴실   2024 / 10 / 9 / 16:30',
                                   style: TextStyle(color: Colors.white)),
-                              SizedBox(height: 10),
+                              SizedBox(height: 10.h),
                               Text('일정 9',
                                   style: TextStyle(
                                       color: Colors.white,
@@ -399,19 +393,18 @@ class _StudentDataTableState extends State<ParticipationStatus> {
                               Divider(),
                               Text('입실   ' + e.time,
                                   style: TextStyle(color: Colors.white)),
-                              SizedBox(height: 5),
+                              SizedBox(height: 5.h),
                               Text('퇴실   2024 / 10 / 9 / 16:30',
                                   style: TextStyle(color: Colors.white)),
-                              SizedBox(height: 10),
+                              SizedBox(height: 10.h),
                             ],
                           ),
                         ),
                       ),
                       actions: [
                         TextButton(
-                          child: Text('닫기',
-                              style: TextStyle(
-                                  color: Colors.white)),
+                          child:
+                              Text('닫기', style: TextStyle(color: Colors.white)),
                           onPressed: () {
                             Navigator.of(context).pop();
                           },
