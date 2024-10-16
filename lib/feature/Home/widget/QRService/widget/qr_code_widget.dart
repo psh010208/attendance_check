637eac6d8fb3f27403f73073b3a300a1d5991c61
psh <psh010208@naver.com>
2024-10-17 01:26:38 +0900

    지희님이 QR 디자인 수정

--- lib/feature/Home/widget/QRService/widget/qr_code_widget.dart
+++ lib/feature/Home/widget/QRService/widget/qr_code_widget.dart
@@ -14,20 +14,22 @@ class QrCodeTile extends StatelessWidget {
 
   Future<void> _saveQrCodeAsPng(BuildContext context, String qrCode) async {
     try {
-      // QR 코드 이미지를 생성
+// QR 코드 이미지를 생성
       final qrImage = await QrPainter(
         data: qrCode,
         version: QrVersions.auto,
         gapless: false,
-        color: Colors.black, // 전경색을 검정색으로 설정
+        color: Colors.black,
+        // 전경색을 검정색으로 설정
         emptyColor: Colors.white, // 배경색을 흰색으로 설정
       ).toImage(500);
 
-      // 이미지 데이터를 PNG로 변환
-      final ByteData? byteData = await qrImage.toByteData(format: ui.ImageByteFormat.png);
+// 이미지 데이터를 PNG로 변환
+      final ByteData? byteData =
+          await qrImage.toByteData(format: ui.ImageByteFormat.png);
       final Uint8List pngBytes = byteData!.buffer.asUint8List();
 
-      // image_gallery_saver를 사용해 갤러리에 저장
+// image_gallery_saver를 사용해 갤러리에 저장
       final result = await ImageGallerySaver.saveImage(
         Uint8List.fromList(pngBytes), // 이미지 데이터를 Uint8List로 변환
         quality: 100, // 이미지 품질 설정
@@ -35,7 +37,7 @@ class QrCodeTile extends StatelessWidget {
       );
 
       if (result['isSuccess']) {
-        // 성공적으로 저장되었을 때 알림 표시
+// 성공적으로 저장되었을 때 알림 표시
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('QR 코드가 갤러리에 저장되었습니다.')),
         );
@@ -67,6 +69,7 @@ class QrCodeTile extends StatelessWidget {
       ),
     );
   }
+
   void _showQrCodeDialog(BuildContext context, String qrCode) {
     showDialog(
       context: context,
@@ -80,7 +83,6 @@ class QrCodeTile extends StatelessWidget {
               painter: QrPainter(
                 data: qrCode,
                 version: QrVersions.auto,
-
                 gapless: false,
               ),
             ),
@@ -88,7 +90,10 @@ class QrCodeTile extends StatelessWidget {
           actions: [
             TextButton(
               onPressed: () => Navigator.of(context).pop(),
-              child: Text('닫기'),
+              child: Text('닫기',
+                  style: TextStyle(
+                      fontSize: 18,
+                      color: Theme.of(context).colorScheme.shadow)),
             ),
           ],
         );
@@ -100,7 +105,11 @@ class QrCodeTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        border: Border.all(color: Theme.of(context).colorScheme.shadow, width: 3.5.w,strokeAlign: BorderSide.strokeAlignOutside), // Border 색상 설정
+        border: Border.all(
+            color: Theme.of(context).colorScheme.shadow,
+            width: 2.w,
+            strokeAlign: BorderSide.strokeAlignOutside),
+        // Border 색상 설정
         borderRadius: BorderRadius.circular(10), // 모서리 둥글게 설정
       ),
       margin: EdgeInsets.symmetric(vertical: 4, horizontal: 4), // 컨테이너 외부 여백
@@ -114,22 +123,33 @@ class QrCodeTile extends StatelessWidget {
             painter: QrPainter(
               data: qrCode,
               version: QrVersions.auto,
-
               gapless: false,
             ),
           ),
         ),
         title: Text(
           scheduleName,
-          style: TextStyle(color: Theme.of(context).colorScheme.shadow, fontWeight: FontWeight.bold), // 일정 이름 색상 설정
+          style: TextStyle(
+              fontSize: 18,
+              color: Theme.of(context).colorScheme.shadow,
+              fontWeight: FontWeight.bold), // 일정 이름 색상 설정
         ),
-        subtitle: Text(
-          'QR 코드: $qrCode',
-          style: TextStyle(color: Theme.of(context).colorScheme.shadow,fontWeight: FontWeight.bold), // QR 코드 텍스트 색상 설정
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start, // 좌측 정렬
+          children: [
+            SizedBox(height: 6),
+            Text(
+              '- QR 코드: $qrCode',
+              style: TextStyle(
+                fontSize: 13,
+                color: Theme.of(context).colorScheme.shadow,
+                fontWeight: FontWeight.bold,
+              ), // QR 코드 텍스트 색상 설정
+            ),
+          ],
         ),
         trailing: IconButton(
-          icon: Icon(Icons.save_alt),
-
+          icon: Icon(Icons.save_alt, size: 30),
           color: Theme.of(context).colorScheme.shadow,
           onPressed: () => _saveQrCodeAsPng(context, qrCode),
         ),
