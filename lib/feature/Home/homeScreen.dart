30f032a3d7bb081d71d09815552d0db5163845fa
psh <psh010208@naver.com>
2024-10-16 23:21:02 +0900

    QR버튼 하단 중앙 정렬

--- lib/feature/Home/homeScreen.dart
+++ lib/feature/Home/homeScreen.dart
@@ -99,29 +99,31 @@ class HomeScreen extends HookWidget {
               // 일정 카드 표시
               buildScheduleCard(context),
 
-              // '학부생' 역할인 경우 QR 코드 스캐너 애니메이션 버튼 추가
+              // QR 코드 스캐너 버튼을 하단 중앙에 배치
               if (role == '학부생')
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: animationButton(
-                    icon: Icons.qr_code_scanner, // QR 코드 아이콘 직접 사용
-                    iconSize: 40, // 아이콘 크기 설정
-                    iconColor: Theme.of(context).colorScheme.scrim, // 아이콘 색상 설정
-                    defaultSize: const Offset(80, 80), // 버튼 기본 크기 설정
-                    clickedSize: const Offset(70, 70), // 버튼 클릭 시 크기
-                    defaultButtonColor: Theme.of(context).colorScheme.onSurface.withOpacity(0.1), // 버튼 색상
-                    clickedButtonColor: Theme.of(context).colorScheme.primary, // 클릭 시 버튼 색상
-                    circularRadius: 50,
-                    onTap: () {
-
-                      // QrScanner로 이동하면서 student_id를 넘김
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => QrScanner(studentId: id), // studentId 전달
-                        ),
-                      );
-                    },
+                Align(
+                  alignment: Alignment.bottomCenter, // 하단 중앙 정렬
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: animationButton(
+                      icon: Icons.qr_code_scanner, // QR 코드 아이콘 직접 사용
+                      iconSize: 40, // 아이콘 크기 설정
+                      iconColor: Theme.of(context).colorScheme.scrim, // 아이콘 색상 설정
+                      defaultSize: const Offset(80, 80), // 버튼 기본 크기 설정
+                      clickedSize: const Offset(70, 70), // 버튼 클릭 시 크기
+                      defaultButtonColor: Theme.of(context).colorScheme.onSurface.withOpacity(0.1), // 버튼 색상
+                      clickedButtonColor: Theme.of(context).colorScheme.primary, // 클릭 시 버튼 색상
+                      circularRadius: 50,
+                      onTap: () {
+                        // QrScanner로 이동하면서 student_id를 넘김
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => QrScanner(studentId: id), // studentId 전달
+                          ),
+                        );
+                      },
+                    ),
                   ),
                 ),
             ],
