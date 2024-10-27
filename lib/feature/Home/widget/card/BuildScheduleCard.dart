7d12306fbd6d64c3bcb660a93f57ea20d65b6fcb
HE <116543097+hekim-cse@users.noreply.github.com>
2024-10-28 07:24:04 +0900

    카드 스크롤

--- lib/feature/Home/widget/card/BuildScheduleCard.dart
+++ lib/feature/Home/widget/card/BuildScheduleCard.dart
@@ -18,11 +18,24 @@ class _ScheduleCardState extends State<Buildschedulecard> {
   late List<bool> isExpandedList;
   double? cardHeight;
   double? cardWidth;
+  late ScrollController _scrollController;
 
   @override
   void initState() {
     super.initState();
-    isExpandedList = List.generate(widget.schedules.length, (index) => false); // 초기화
+    isExpandedList = List.generate(widget.schedules.length, (index) => false);
+    _scrollController = ScrollController();
+
+    _scrollController.addListener(() {
+      print("Scroll Position: ${_scrollController.position.pixels}");
+      print("Max Scroll Extent: ${_scrollController.position.maxScrollExtent}");
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 
   @override
@@ -42,50 +55,88 @@ class _ScheduleCardState extends State<Buildschedulecard> {
       Theme.of(context).disabledColor,
     ];
 
-    return SingleChildScrollView(
-      child: Center(
-        child: Stack(
-          alignment: Alignment.topCenter,
-          children: [
-            Column(
-              children: [
-                Container(
-                  height: isExpandedList.contains(true)
-                      ? MediaQuery.of(context).size.height * 0.025.h
-                      : MediaQuery.of(context).size.height * 0.22.h,
-                ),
-                Stack(
-                  clipBehavior: Clip.none,
-                  alignment: Alignment.topCenter,
-                  children: buildOverlappingCards(context, widget.schedules, barColors),
-                ),
-              ],
-            ),
-            AnimatedPositioned(
-              top: isExpandedList.contains(true) ? -MediaQuery.of(context).size.height : -0.5, // 카드가 펼쳐지면 위로 이동
-              duration: Duration(milliseconds: 400), // 애니메이션 지속 시간
-              curve: Curves.easeInOut, // 애니메이션 곡선
-              child: AnimatedOpacity(
-                opacity: isExpandedList.contains(true) ? 0 : 1, // 카드가 펼쳐지면 숨김
-                duration: Duration(milliseconds: 200), // 애니메이션 지속 시간
-                child: Container(
-                  width: MediaQuery.of(context).size.width, // 원하는 너비 지정
-                  height: MediaQuery.of(context).size.height, // 원하는 높이 지정
-                  child: SoonCheckWidget(
-                    bottom: MediaQuery.of(context).size.height * 0.85, // 비례 조정
-                    left: MediaQuery.of(context).size.width * 0.21, // 원하는 SoonCheckWidget 추가
+    return Container(
+      child: Column(
+        children: [
+          Expanded(
+            child: Container(
+              child: Container(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    maxHeight: MediaQuery.of(context).size.height * 0.8, // 최대 높이 설정
+                  ),
+                  child: SingleChildScrollView(
+                    controller: _scrollController,
+                    physics: isExpandedList.contains(true)
+                        ? AlwaysScrollableScrollPhysics()
+                        : NeverScrollableScrollPhysics(),
+                    child: Center(
+                      child: Container(
+                        child: Stack(
+                          alignment: Alignment.topCenter,
+                          children: [
+                            Container(
+                              child: Column(
+                                children: [
+                                  Container(
+                                    height: isExpandedList.contains(true)
+                                        ? MediaQuery.of(context).size.height * 0.025.h
+                                        : MediaQuery.of(context).size.height * 0.22.h,
+                                  ),
+                                  Container(
+                                    child: Stack(
+                                      clipBehavior: Clip.none,
+                                      alignment: Alignment.topCenter,
+                                      children: buildOverlappingCards(
+                                        context,
+                                        widget.schedules,
+                                        barColors,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            AnimatedPositioned(
+                              top: isExpandedList.contains(true)
+                                  ? -MediaQuery.of(context).size.height
+                                  : -0.5,
+                              duration: Duration(milliseconds: 400),
+                              curve: Curves.easeInOut,
+                              child: AnimatedOpacity(
+                                opacity: isExpandedList.contains(true) ? 0 : 1,
+                                duration: Duration(milliseconds: 200),
+                                child: Container(
+                                  width: MediaQuery.of(context).size.width,
+                                  height: MediaQuery.of(context).size.height,
+                                  child: SoonCheckWidget(
+                                    bottom: MediaQuery.of(context).size.height * 0.85,
+                                    left: MediaQuery.of(context).size.width * 0.21,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               ),
             ),
-
-          ],
-        ),
+          ),
+          Container(
+            child: SizedBox(
+              height: isExpandedList.contains(true) ? 80 : 0, // 펼쳐졌을 때만 공간 차지
+            ),
+          ),
+        ],
       ),
     );
   }
 
-  List<Widget> buildOverlappingCards(BuildContext context, List<Schedule> schedules, List<Color> barColors) {
+
+    List<Widget> buildOverlappingCards(BuildContext context, List<Schedule> schedules, List<Color> barColors) {
     return schedules.asMap().entries.map((entry) {
       int index = entry.key;
       Schedule schedule = entry.value;
@@ -97,11 +148,13 @@ class _ScheduleCardState extends State<Buildschedulecard> {
     return GestureDetector(
       onVerticalDragUpdate: (details) {
         setState(() {
+          bool isAtBottom = _scrollController.position.pixels >= _scrollController.position.maxScrollExtent;
+
           if (details.delta.dy > 10) {
             for (int i = 0; i < isExpandedList.length; i++) {
               isExpandedList[i] = true;
             }
-          } else if (details.delta.dy < -10) {
+          } else if (details.delta.dy < -10 && isAtBottom) {
             for (int i = 0; i < isExpandedList.length; i++) {
               isExpandedList[i] = false;
             }
@@ -134,8 +187,8 @@ class _ScheduleCardState extends State<Buildschedulecard> {
         isExpanded: isExpanded,
         onSizeCalculated: (height, width) {
           setState(() {
-            cardHeight = height;
-            cardWidth = width;
+            cardHeight = height; // 카드 높이 저장
+            cardWidth = width;   // 카드 너비 저장
           });
         },
       ),
